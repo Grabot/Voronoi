8d19c7c9eabc5b69445a3e6794c3712074573e01
Grabot <SanderKools@gmail.com>
2015-11-23 18:34:14 +0100

    I made some begin for the game

---
+++ Voronoi/Assets/Graph.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Graph : MonoBehaviour
+{
+    public float m_Radius = 5f;
+    List<Vector2> vertices = new List<Vector2>();
+
+    void Awake()
+    {
+        createVertices();
+    }
+
+    private void createVertices()
+    {
+        vertices.Add(new Vector2(3, 3));
+        GameObject gob = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        gob.transform.position = vertices[0];
+        gob.transform.localScale = new Vector3(m_Radius, m_Radius, m_Radius);
+            
+    }
+
+    void OnMouseDown()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+        if (hits.Length > 0)
+        {
+            Vector3 newPos = hits[0].point;
+            vertices.Add(newPos);
+
+            GameObject gob = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            gob.transform.position = newPos;
+            gob.transform.localScale = new Vector3(m_Radius, m_Radius, m_Radius);
+        }
+    }
+}
---
+++ Voronoi/Assets/Node.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node {
+
+    Vector3 m_WorldPos;
+
+    public Node( Vector3 a_WorldPos)
+    {
+        m_WorldPos = a_WorldPos;
+    }
+}
